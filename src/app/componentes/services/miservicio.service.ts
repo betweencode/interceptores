e1f8e70c1163c771bf9c656342ff362556c4d4c2
samv <mariscal@gmail.com>
2022-08-10 11:17:08 -0500

    samv

--- src/app/componentes/services/miservicio.service.ts
+++ src/app/componentes/services/miservicio.service.ts
@@ -10,14 +10,8 @@ export class MiservicioService {
   constructor(private http:HttpClient) { }
 
   public getLista():Observable<any>{
-      const httpoption = {
-        headers: new HttpHeaders({
-          'Content-Type':"application/json",
-          'personalizado':'valorx',
-          'auth':'token'
-        })
-      }
-      return this.http.get("http://list.ly/api/v4/lists/1",httpoption).pipe(map((s:any)=>s.list));
+      
+      return this.http.get("http://list.ly/api/v4/lists/1").pipe(map((s:any)=>s.list));
   }
 
   
